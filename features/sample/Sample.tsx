5fa99978fc44df2e6da47bbc846d854f573251c9
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-07-02 11:02:46 +0900

    [fix]分岐最適化

--- features/sample/Sample.tsx
+++ features/sample/Sample.tsx
@@ -3,24 +3,25 @@ import { fetchHello } from './fetchPosts';
 
 export default async function Sample() {
     const { data, error } = await fetchHello();
+
+    if (!data) {
+        return (
+            <div>
+                {error === 404 ? (
+                    <>URLが間違っております</>
+                ) : (
+                    error === 500 && <>時間をおいて再度訪問してください</>
+                )}
+            </div>
+        );
+    }
+
     console.log('json placeholderのデータ', data);
     return (
         <div className="text-white">
             <div>
-                <>{data?.title}</>
+                <>{data.title}</>
             </div>
-
-            {data === null && (
-                <div>
-                    {error.status === 404 ? (
-                        <>URLが間違っております</>
-                    ) : error.status === 500 ? (
-                        <>時間をおいて再度訪問してください</>
-                    ) : (
-                        <>{error.message}</>
-                    )}
-                </div>
-            )}
         </div>
     );
 }
